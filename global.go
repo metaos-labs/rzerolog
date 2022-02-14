f082dff9938ab31d99250036bec2aa75c59d22d5
ramboll <ramboll@live.com>
2022-02-14 17:57:36 +0800

    fix default time format

--- global.go
+++ global.go
@@ -5,7 +5,7 @@ const (
 )
 
 const (
-	DefaultTimeFormat   = "2006-01-02 15:04:05.000 MST Z0700"
+	DefaultTimeFormat   = "2006-01-02 15:04:05.000"
 	DefaultFilePath     = "./"
 	DefaultFileName     = "RZero.log"
 	DefaultFileSize     = 100 * 1 << 10 // 100M
